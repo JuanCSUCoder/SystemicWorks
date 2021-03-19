9a89a59d5b6c061f03963a623d89492ab910ea86
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-03-19 17:52:32 -0500

    Change grid scale

--- js/Model.js
+++ js/Model.js
@@ -148,12 +148,12 @@ function Model(loopy){
 
 	var grid_svg_data = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"> \
 			<defs> \
-					<pattern id="smallGrid" width="30" height="30" patternUnits="userSpaceOnUse"> \
-							<path d="M 30 0 L 0 0 0 30" fill="none" stroke="gray" stroke-width="2" /> \
+					<pattern id="smallGrid" width="50" height="50" patternUnits="userSpaceOnUse"> \
+							<path d="M 50 0 L 0 0 0 50" fill="none" stroke="gray" stroke-width="2" /> \
 					</pattern> \
 					<pattern id="grid" width="80" height="80" patternUnits="userSpaceOnUse"> \
 							<rect width="80" height="80" fill="url(#smallGrid)" /> \
-							<path d="M 80 0 L 0 0 0 80" fill="none" stroke="black" stroke-width="1" /> \
+							<path d="M 80 0 L 0 0 0 80" fill="none" stroke="grey" stroke-width="2" /> \
 					</pattern> \
 			</defs> \
 			<rect width="100%" height="100%" fill="url(#smallGrid)" /> \
