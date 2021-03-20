e0a68cf7ed86285a6cdf8c919ef0ce6e8a01e029
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-03-20 18:23:03 -0500

    Make a Better Implementation of Panning

--- js/Pan.js
+++ js/Pan.js
@@ -16,24 +16,27 @@ function Pan(loopy) {
 	subscribe("mousedown", function () {
 		self.active = true;
 		
-		self.mouseXRef = Mouse.x;
-		self.mouseYRef = Mouse.y;
+		self.mouseXRef = Mouse.x - self.canvasX;
+		self.mouseYRef = Mouse.y - self.canvasY;
+
+		loopy.initialCanvasX = self.canvasX;
+		loopy.initialCanvasY = self.canvasY;
 	})
 
 	subscribe("mouseup", function () {
 		self.active = false;
+
+		loopy.initialCanvasX = self.canvasX;
+		loopy.initialCanvasY = self.canvasY;
 	})
 
 	subscribe("mousemove", function () {
 		if(window.loopy.tool == Loopy.TOOL_PAN && self.active) {
-			self.canvasX += Mouse.x - self.mouseXRef;
-			self.canvasY += Mouse.y - self.mouseYRef;
+			self.canvasX = Mouse.x - self.mouseXRef;
+			self.canvasY = Mouse.y - self.mouseYRef;
 
 			loopy.canvasX = self.canvasX;
 			loopy.canvasY = self.canvasY;
-
-			self.mouseXRef = Mouse.x;
-			self.mouseYRef = Mouse.y;
 
 			publish("canvas/moved");
 		}
