01b844e2a4713c28a47e3735cdb6b1aeaecd9d38
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-03-31 15:33:22 -0500

    Add Drawing and Helper Method

--- js/LoopMark.js
+++ js/LoopMark.js
@@ -35,10 +35,37 @@ function LoopMark(model, config) {
 
 		ctx.beginPath();
 
+		ctx.strokeStyle = self.color;
+		ctx.lineWidth = 4*2;
+
 		ctx.arc(0, 0, 20 * 2, -Math.PI*5/4, Math.PI*2/5, !self.clockwise);
 
 		ctx.stroke();
 
+		ctx.font = "100 " + Label.FONTSIZE + "px sans-serif";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = self.color;
+
+		if (self.reinforcement) {
+			ctx.fillText("R", 0, 0);
+		} else {
+			ctx.fillText("B", 0, 0);
+		}
+
 		ctx.restore();
+	}
+
+	////////////////////
+	// HELPER METHODS //
+	////////////////////
+
+	self.isPointInLoopMark = function (x, y) {
+		var dx = self.x - x;
+		var dy = self.y - y;
+
+		var d = Math.sqrt(dx * dx + dy * dy);
+
+		return d <= 20;
 	}
 }
