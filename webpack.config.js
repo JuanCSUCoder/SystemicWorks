daca27b348d26d783816aad9b0991de38b1351bc
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-04-24 23:39:03 +0000

    Change Output Folder

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	mode: 'development',
 	entry: './src/index.ts',
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'build'),
 	},
 	devServer: {
 		open: true,
