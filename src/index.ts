103f22da5b2bc9c7db0174903f0188d0679db16f
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-06-17 00:07:15 +0000

    Fixes Make the sidebar collapsible and optimize it for mobile [3] #56

--- src/index.ts
+++ src/index.ts
@@ -20,10 +20,30 @@ window.publish = publish;
 window.onload = function () {
 	window.loopy = new Loopy();
 
-	var goto_element: HTMLElement = document.getElementById("goto");
+	let goto_element: HTMLElement = document.getElementById("goto");
 	goto_element.addEventListener("click", () => {
 		toggleStylesheet("assets/fs.css");
 	});
 
-	var KeysController = new Key(window, window.loopy);
+	let sidebar: HTMLElement = document.getElementById("sidebar");
+	let canvas_cont: HTMLElement = document.getElementById("canvasses");
+
+	let hide_button: HTMLElement = document.getElementById("hide-button");
+	let unhide_button: HTMLElement = document.getElementById("unhide-button");
+
+	hide_button.addEventListener("click", () => {
+		sidebar.style.display = "none";
+		canvas_cont.style.height = "100%";
+		publish("resize");
+		unhide_button.style.display = "flex";
+	});
+
+	unhide_button.addEventListener("click", () => {
+		sidebar.style.display = "block";
+		canvas_cont.style.removeProperty("height");
+    publish("resize");
+    unhide_button.style.display = "none";
+	});
+
+	let KeysController = new Key(window, window.loopy);
 };
