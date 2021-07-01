69d6d73fe823df698aa2faa7363e9482888f962f
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-07-01 22:41:16 +0000

    Removes old helper function (patch)

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -295,17 +295,6 @@ export function _makeErrorFunc(msg) {
 	};
 }
 
-export function _getParameterByName(name) {
-	var url = window.location.href;
-	name = name.replace(/[\[\]]/g, "\\$&");
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-		results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return '';
-	return decodeURIComponent(results[2].replace(/\+/g, " "));
-};
-
-
 export function _blendColors(hex1, hex2, blend) {
 
 	var color = "#";
